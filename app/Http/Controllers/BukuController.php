a007629251b684a36dc5af905f6d2a5f519c2f3d
RyhHan <amuhraihan2324@gmail.com>
2024-10-09 17:15:59 +0700

    modul4 + challenge

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Buku;
 use App\Http\Requests\StoreBukuRequest;
 use App\Http\Requests\UpdateBukuRequest;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Session\TokenMismatchException;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class BukuController extends Controller
 {
@@ -13,7 +17,9 @@ class BukuController extends Controller
      */
     public function index()
     {
-        return view('buku/index');
+        return view('buku/index',[
+            'bukus' => DB::table('bukus')->get()
+        ]);
     }
 
     /**
@@ -21,6 +27,7 @@ class BukuController extends Controller
      */
     public function create()
     {
+        
         return view('buku/create');
     }
 
@@ -39,6 +46,7 @@ class BukuController extends Controller
             $ValidatedData['sampul'] = $request->file('sampul')->store('/sampul-buku');
             }
             Buku::create($ValidatedData);
+            Alert::success("BERHASIL",'tambah data berhasil dilakukan');
             return redirect('/buku');
     }
 
@@ -53,24 +61,52 @@ class BukuController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Buku $buku)
+    public function edit($id)
     {
-        //
+        $test = DB::table('bukus')->where('id',$id)->get();
+        return view('buku/update',[
+            'buku' => $test[0]
+        ]);
+        
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateBukuRequest $request, Buku $buku)
+    public function update(UpdateBukuRequest $request, $id)
     {
-        //
+        $ValidatedData = $request->validate([
+            'judul'=>'required',
+            'penulis'=>'required',
+            'kategori'=>'required',
+            'sampul'=>'image|file|max:2048',        
+        ]);
+
+        if ($request->file('sampul')) {
+            if ($request->sampulLama) {
+                Storage::delete($request->sampulLama);
+            }
+            $ValidatedData['sampul'] = $request->file('sampul')->store('/sampul-buku');
+        }
+        Buku::where('id',$id)->update($ValidatedData);
+        Alert::success("BERHASIL",'Edit berhasil dilakukan');
+
+        return redirect('/buku');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Buku $buku)
+    public function destroy($id)
     {
-        //
+        $test = DB::table('bukus')->select('sampul')
+        ->where('id',$id)
+        ->get();
+        if ($test[0]->sampul) {
+            Storage::delete($test[0]->sampul);
+        }
+        Buku::destroy($id);
+        Alert::success("BERHASIL",'hapus berhasil dilakukan');
+        return redirect('/buku');
     }
 }
